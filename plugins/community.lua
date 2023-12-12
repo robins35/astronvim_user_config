21f6734cee605124d5237393ee4484dfdb98b6f5
Caiden Robinson <caiden.robinson35@gmail.com>
2023-12-12 09:11:19 -0800

    Adds and configures the copilot.lua community plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,65 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require('copilot').setup({
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-m>",
+          -- accept = "<C-a>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          -- next = "<C-d>",
+          prev = "<M-[>",
+          -- prev = "<C-u>",
+          dismiss = "<C-c>",
+        },
+      },
+      filetypes = {
+        ruby = true,
+        rust = true,
+        vue = true,
+        javascript = true,
+        typescript = true,
+        go = true,
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 18.x
+      server_opts_overrides = {},
+    })
+  end,
+  }
 }
